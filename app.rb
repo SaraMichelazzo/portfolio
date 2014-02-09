775a0381c7be3a533ab6824678d68011cf4dd617
albertodotcom <alberto.forn@gmail.com>
2014-02-09 22:13:11 +1100

    Sinatra app

---
+++ app.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+
+if development?
+  require 'sinatra/reloader'
+end
+
+get '/' do
+  @page = 'work'
+  erb :index
+end
+
+get '/index' do
+  redirect '/'
+end
+
+get '/about' do
+  @page = 'about'
+  erb :about
+end
+
+get '/SolairPlmUX' do
+  @page = 'work'
+  erb :SolairPlmUX
+end
+
+get '/linkedin' do
+  @page = 'work'
+  erb :linkedin
+end
+
+get '/SolairSite' do
+  @page = 'work'
+  erb :SolairSite
+end
+
+get '/truespirit' do
+  @page = 'work'
+  erb :truespirit
+end
